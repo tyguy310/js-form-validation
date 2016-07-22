94758e7c084d8f4983adff3d0f5f9149285ecff7
Tyler Maier <tyler_maier@yahoo.com>
2016-07-22 08:23:25 -0600

    fix code style errors

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function()  {
-  console.log("sanity check");
+  console.log('sanity check');
 
   $('form').on('submit', function(event)  {
     event.preventDefault();
@@ -8,20 +8,20 @@ $(document).ready(function()  {
     var lastName = $('#lastName').val();
     var eMail = $('#email').val();
 
-    if (firstLetterCapital(firstName)){
-      showCallout("Capitalize the first letter")
+    if (firstLetterCapital(firstName)) {
+      showCallout('Capitalize the first letter');
     }
 
-    if (firstLetterCapital(lastName)){
-      showCallout("Capitalize the first letter")
+    if (firstLetterCapital(lastName)) {
+      showCallout('Capitalize the first letter');
     }
 
     if (validateFirst(firstName)) {
-      showCallout("Your name is too short!");
+      showCallout('Your name is too short!');
     }
 
     if (validateEmail(eMail)) {
-      showCallout("Enter a valid email address");
+      showCallout('Enter a valid email address');
     }
   });
 });
@@ -30,21 +30,20 @@ function hideCallout()  {
 }
 
 function firstLetterCapital(name) {
-  return (name[0] !== name[0].toUpperCase()) ? true:false;
+  return (name[0] !== name[0].toUpperCase()) ? true : false;
 }
 
 function  validateEmail(email)  {
-  return (email.indexOf('@')+1 === email.lastIndexOf('.'))
-  || (email.indexOf('@')<=0)  ? true:false
+  return (email.indexOf('@') + 1 === email.lastIndexOf('.')) || (email.indexOf('@') <= 0)  ? true : false;
 }
 
- function showCallout(msg)  {
-   $('.valid-callout > h2').text(msg);
-   $('.valid-callout').css('visibility', 'visible');
-   setTimeout(hideCallout, 2000);
- }
+function showCallout(msg)  {
+  $('.valid-callout > h2').text(msg);
+  $('.valid-callout').css('visibility', 'visible');
+  setTimeout(hideCallout, 2000);
+}
 
- function validateFirst(name) {
-   var minValidLetters = 3;
-   return (name.length < minValidLetters) ? true : false;
- }
+function validateFirst(name) {
+  var minValidLetters = 3;
+  return (name.length < minValidLetters) ? true : false;
+}
